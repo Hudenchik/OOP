3b21e278d9f8c1268367a57eeb64ed3083d35b58
Hudenchik <den363530@gmail.com>
2025-02-27 16:20:35 +0500

    Половина работы
    Добавлена возможность создавать объекты класса "CCircle", добавлять их в контейнер-объект класса "MyStorage" и отрисовывать их на фрейме.

--- LR3/LR3/mainwindow.cpp
+++ LR3/LR3/mainwindow.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,7 +8,84 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 }
 
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void CCircle::draw(QPainter &painter, QRect restriction)
+{
+    painter.setClipRect(restriction);
+    painter.setBrush(Qt::blue);
+    painter.drawEllipse(x - rad, y - rad, 2 * rad, 2 * rad);
+}
+
+bool CCircle::contains(int px, int py)
+{
+    return (px - x) * (px - x) + (py - y) * (py - y) <= rad*rad;
+}
+
+
+void MyStorage::AddObject(int x, int y)
+{
+    circles.push_back(new CCircle(x, y));
+}
+
+void MyStorage::drawCircles(QPainter &painter, QRect restriction)
+{
+    for (size_t i = 0; i<circles.size();i++){
+        circles[i]->draw(painter, restriction);
+    }
+}
+
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    storage.drawCircles(painter, ui->frame->geometry());
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        QRect frameGeometry = ui->frame->geometry();
+        QPoint localPos = event->pos() - frameGeometry.topLeft();
+
+        int cornerThreshold = 20;
+
+        bool topLeft = (localPos.x() < cornerThreshold && localPos.y() < cornerThreshold);
+        bool topRight = (localPos.x() > frameGeometry.width() - cornerThreshold && localPos.y() < cornerThreshold);
+        bool bottomLeft = (localPos.x() < cornerThreshold && localPos.y() > frameGeometry.height() - cornerThreshold);
+        bool bottomRight = (localPos.x() > frameGeometry.width() - cornerThreshold && localPos.y() > frameGeometry.height() - cornerThreshold);
+
+
+        if (topLeft || topRight || bottomLeft || bottomRight)
+        {
+            isResizing = true;
+            lastMousePosition = event->pos();
+            return;
+        }
+
+        storage.AddObject(event->pos().x(), event->pos().y());
+        update();
+
+    }
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (isResizing) {
+        int dx = event->position().x() - lastMousePosition.x();
+        int dy = event->position().y() - lastMousePosition.y();
+
+        ui->frame->resize(ui->frame->width() + dx, ui->frame->height() + dy);
+        lastMousePosition = event->pos();
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    isResizing = false;
+}
+
+
--- LR3/LR3/mainwindow.h
+++ LR3/LR3/mainwindow.h
@@ -2,12 +2,47 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
+#include <QMouseEvent>
+
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+
+class CCircle{
+private:
+    int x, y;
+    const int rad = 40;
+
+public:
+    CCircle(int ValueX, int ValueY):x(ValueX), y(ValueY){}
+    void draw(QPainter &painter, QRect restriction);
+    bool contains(int px, int py);
+
+};
+
+
+class MyStorage{
+private:
+    vector<CCircle*> circles;
+    vector<CCircle*> selectedCircles;
+public:
+
+    void AddObject(int x, int y);
+
+    void drawCircles(QPainter &painter, QRect restriction);
+
+    ~MyStorage(){
+        for (size_t i = 0; i<circles.size();i++){
+            delete circles[i];
+        }
+    }
+};
 
 class MainWindow : public QMainWindow
 {
@@ -17,7 +52,18 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+
 private:
     Ui::MainWindow *ui;
+    MyStorage storage;
+    bool isResizing = false;
+    QPoint lastMousePosition;
 };
+
+
 #endif // MAINWINDOW_H
